ceba3bed962638ee4a09322e5e1301208a95d920
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-02 14:04:14 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,13 @@
+ unction getHousesPrototype() {  
+ const HousesMap = new Map();  
+ 
+ HousesMap.set("Apartment 75008", 90);
+ HousesMap.set("Apartment 94300", 105);
+ HousesMap.set("Apartment 92300", 140);
+ HousesMap.set("Apartment 75015", 65);
+ return HousesMap;
+ }
+ console.log(getHousesPrototype() ) ;
+
+
+ //ne s'affiche pas dans jsffidle
