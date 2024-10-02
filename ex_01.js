11a8d376ae2f51b57d62ada0a0b1d2475a734585
sarah1reindl <sarah1.reindl@epitech.digital>
2024-10-02 10:12:49 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,10 @@
+function getAngryDog(numberOfWoofs)
+{    
+for (i = 0; i < numberOfWoofs; i++) result += "woof";
+    return result;
+}
+console.log("displayResult(getAngryDog(5))")
+
+//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
+var result = getAngryDog(5);
+displayResult(result);
